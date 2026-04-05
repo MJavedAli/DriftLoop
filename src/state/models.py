929b547f25eeb46c968f630567c5f5a36809d120
Javed Ali <30997178+MJavedAli@users.noreply.github.com>
2026-04-05 13:53:37 +0530

    Update models.py

--- src/state/models.py
+++ src/state/models.py
@@ -1,1 +1,19 @@
+from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy.orm import declarative_base
 
+Base = declarative_base()
+
+class Change(Base):
+    __tablename__ = "changes"
+    id = Column(Integer, primary_key=True)
+    commit = Column(String)
+    service = Column(String)
+    timestamp = Column(String)
+    summary = Column(String)
+
+class Metric(Base):
+    __tablename__ = "metrics"
+    id = Column(Integer, primary_key=True)
+    service = Column(String)
+    timestamp = Column(String)
+    value = Column(Float)
